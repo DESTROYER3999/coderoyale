c193c8344c39ca259899b7529c33f600805ee768
DESTROYER3999 <ianbhatt@gmail.com>
2021-02-07 09:10:02 -0500

    Fixed all html and styling

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,18 @@
 const socket = io();
 
-const runButton = document.getElementById("run-btn");
+// const runButton = document.getElementById("run-btn");
 const outputArea = document.getElementById("output");
-const leftDiv  = document.getElementById("left");
-const rightDiv = document.getElementById("right");
-const monacoEditor = document.getElementById('monaco-container');
+const leftDiv  = document.getElementsByClassName("left")[0];
+const rightDiv = document.getElementsByClassName("right")[0];
+const monacoContainer = document.getElementById('monaco-container');
 const contentDiv = document.getElementById("maincontent");
 const mdeElement = document.getElementById("simplemde-editor");
-// const inputArea = document.getElementById("input");
-runButton.addEventListener("click", run_clicked);
+const windows = document.getElementsByClassName("window");
+const verticalSeperator = document.getElementById("vertical-seperator");
+const horizontalSeperator = document.getElementById("horizontal-seperator");
+const outputDiv = document.getElementById("output-window");
+
+// runButton.addEventListener("click", run_clicked);
 
 require.config({ paths: { vs: 'monaco-editor/min/vs' } });
 
@@ -16,31 +20,62 @@ const editMde = new SimpleMDE({
     element: mdeElement,
     spellChecker: false,
     status: false,
-    hideIcons: ['guide', 'fullscreen', 'side-by-side', "image"]
-});
-  
+    hideIcons: ['guide', 'fullscreen', 'side-by-side'],
+    showIcons: ["code", "strikethrough", "table", "horizontal-rule"]
 
-let editor;
+});
+
+function editor_layout() {
+    editor.layout({
+        width: 0,
+        height: 0
+    });    
+    editor.layout({
+        width: document.body.offsetWidth - verticalSeperator.offsetLeft - 10,
+        height: rightDiv.offsetHeight - outputDiv.offsetHeight - 45
+    });
+
+}
+
+function content_layout() {
+    contentDiv.style.gridTemplateColumns = "1fr auto 1fr";
+}
+
+
+
 require(['vs/editor/editor.main'], function () {
-    editor = monaco.editor.create(monacoEditor, {
+    monaco.editor.defineTheme('my-dark', {
+        base: 'vs-dark', 
+        inherit: true,
+        rules: [{ background: '#444444' }],
+        colors: {
+            "editor.foreground": "#aaaaaa",
+            "editor.background": '#444444',
+            "editor.lineHighlightBorder": "white"
+        }
+    });
+    
+    editor = monaco.editor.create(monacoContainer, {
         value: "",
         language: 'python',
-        automaticLayout: true,
+        automaticLayout: false,
         theme: "vs-dark",
-        fontSize: "14px",
+        fontSize: "16px",
         minimap: {
             enabled: false
         },
         lineNumbersMinChars: 3,
-        lineDecorationsWidth: 3
+        lineDecorationsWidth: 3,
+        top: 5
     });
-
-    // window.onresize = function () {
-    //     editor.layout();
-    // };
-
+    editor_layout();
+    window.addEventListener("resize", editor_layout);
+    window.addEventListener("resize", content_layout);
 
 });
+
+
+
 
 function handle_result(result) {
     console.log("recieved result");
@@ -74,51 +109,100 @@ socket.on("connect", () => {
 
 
 
-function dragElement(element, direction) {
-    var   md;
-    element.onmousedown = onMouseDown;
-    function onMouseDown(e)
-    {
-        if (direction == "V") {
-            md = {e,
-                offsetLeft:  element.offsetLeft,
-                offsetTop:   parseInt(getComputedStyle(element).top.slice(0, -2), 10),
-                firstHeight:  monacoEditor.offsetHeight,
-                secondHeight: outputArea.offsetHeight
-               };
-        } else if (direction == "H") {
-            md = {e,
-                offsetLeft:  element.offsetLeft,
-                offsetTop:   element.offsetTop,
-                firstWidth:  leftDiv.offsetWidth,
-                secondWidth: rightDiv.offsetWidth
-               };
-        }
-  
-
-        document.onmousemove = onMouseMove;
+function handle_drag_seperator(seperator, parent, direction) {
+    seperator.addEventListener("mousedown", (e) => {
+        mouseDownEvent = e;
+        document.onmousemove = (e) => onMouseMove(e, mouseDownEvent);
         document.onmouseup = () => {
             document.onmousemove = document.onmouseup = null;
         }
-    }
+    })
 
-    function onMouseMove(e)
-    {
-        //console.log("mouse move: " + e.clientX);
-        var delta = {x: e.clientX - md.e.clientX,
-                     y: e.clientY - md.e.clientY};
+    function onMouseMove(e, mouseDownEvent) {
+        var delta = {
+            x: e.clientX - mouseDownEvent.clientX, 
+            y: e.clientY - mouseDownEvent.clientY
+        };
+        if (direction == "V") {
+            let minWidthLeft = 5;
+            for (let tab of windows[0].children[0].children) {
+                minWidthLeft += tab.offsetWidth + 5;
+            }
+            minWidthLeft += 2;
+            let tabsWidthRightTop = 5;
+            for (let tab of windows[1].children[0].children) {
+                tabsWidthRightTop += tab.offsetWidth + 5;
+            }
+            tabsWidthRightTop += 2;
+            let tabsWidthRightBottom = 5;
+            for (let tab of windows[2].children[0].children) {
+                tabsWidthRightBottom += tab.offsetWidth + 5;
+            }
+            tabsWidthRightBottom += 2;
+            let minWidthRight = Math.max(tabsWidthRightTop, tabsWidthRightBottom);
+            let newWidth;
+            newWidth = Math.max(mouseDownEvent.clientX + delta.x - 14, minWidthLeft);
+            // contentDiv.offsetWidth - (212 + 5)
+            newWidth = Math.min(contentDiv.offsetWidth - (minWidthRight + 5), newWidth);
+            console.log(newWidth)
+            parent.style.gridTemplateColumns = `${newWidth}px auto 1fr`;
 
-        if (direction == "H" )
-        {
-            contentDiv.style.gridTemplateColumns = (md.offsetLeft + delta.x) + "px 5px 1fr";
-        
+        } else if (direction == "H") {
+            let minWidthTop = 42 * 2;
+            let minWidthBottom = 38 * 2;
+            let newWidth;
+            newWidth = Math.max(window.innerHeight - mouseDownEvent.clientY - delta.y - 22, minWidthBottom - 7);
+            newWidth = Math.min(rightDiv.offsetHeight - (minWidthTop), newWidth + 10);
+            console.log("NEW", newWidth)
+            // contentDiv.offsetWidth - (212 + 5)
+            // newWidth = Math.min(contentDiv.offsetWidth - (minWidthRight + 3), newWidth);
 
-        } else if (direction == "V") {
-
-            rightDiv.style.gridTemplateRows = (md.firstHeight + delta.y) + "px 5px 1fr";
+            parent.style.gridTemplateRows = `1fr auto ${newWidth}px`
         }
+        // console.log(delta);
+        // console.log(`${mouseDownEvent.offsetLeft + delta.x}px auto 1fr`, ":::", parent.style.gridTemplateColumns);
+        // if (direction == "V" )
+        // {
+        //     contentDiv.style.gridTemplateColumns = (md.offsetLeft + delta.x) + "auto 1fr";
+        //     // rightDiv.style.gridTemplateColumns = "1px " + (md.offsetLeft - delta.x) + "px";
+        // } else if (direction == "H") {
+        //     rightDiv.style.gridTemplateRows = (md.firstHeight + delta.y) + "px 1px 1fr";
+            
+        // }
+        editor_layout();
     }
 }
 
-dragElement(document.getElementById("vertical-seperator"), "H");
-dragElement(document.getElementById("horizontal-seperator"), "V");
+handle_drag_seperator(verticalSeperator, contentDiv, "V");
+handle_drag_seperator(horizontalSeperator, rightDiv, "H");
+
+for (let window of windows) {
+    const controlsDiv = window.children[0];
+    const tabs = controlsDiv.children;
+    const pages = window.children[1].children;
+    for (let i = 0; i < tabs.length; i++) {
+        tabs[i].pages = pages[i];
+        tabs[i].addEventListener("click", () => {
+            for (let tab of tabs) {
+                tab.classList.remove("selected");
+                tab.pages.classList.remove("selected");
+            }
+            tabs[i].classList.add("selected");
+            pages[i].classList.add("selected");
+        })
+    }
+}
+
+
+// function left_tab_clicked(childNum) {
+//     for (let i = 0; i < leftDivs.length; i++) {
+//         leftDivs[i].classList.remove("selected");
+//         leftTabs[i].classList.remove("selected");
+//     }
+//     leftDivs[childNum].classList.add("selected");
+//     leftTabs[childNum].classList.add("selected");
+// }
+
+// for (let i = 0; i < leftTabs.length; i++) {
+//     leftTabs[i].addEventListener("click", () => left_tab_clicked(i));
+// }
